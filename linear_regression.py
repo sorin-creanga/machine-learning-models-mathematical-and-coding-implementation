e1967c30c224c209f6a5757c04d9b1cdf6fba7f0
sorin.creanga <screan01@student.bbk.ac.uk>
2025-11-28 15:43:10 +0000

    Adding file description

---
+++ linear_regression.py
@@ -0,0 +1,9 @@
+"""
+    In this python file the aim is to replicate mathematically and manually the work complexity of the LinearRegression() model
+    from the SKLEARN.model library.
+
+    The aim is to develop a deep understating of how the model works under the hood, before rellying on pre-build packages.
+
+"""
+
+
